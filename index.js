d41aa59b8e0fe55f5a89a26788d904ae7e7b4867
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-22 22:09:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-let recipes = {
-  
+var recipes = new Object()
+
+ function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
 }
 
-function updateObjectWithKeyAndValue(object, key, value){
-  
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value
+return object
+}
+
+ function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object)
+  delete newObj[key]
+  return newObj
+
+ }
+
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
 }
